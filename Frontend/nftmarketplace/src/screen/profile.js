f6ffbdf64769a3bbc0039e186f9d1709cf56b3a0
JasonChen990513 <149560212+JasonChen990513@users.noreply.github.com>
2024-12-12 17:21:51 +0800

    fix bug

--- Frontend/nftmarketplace/src/screen/profile.js
+++ Frontend/nftmarketplace/src/screen/profile.js
@@ -242,9 +242,9 @@ const Profile = () => {
             <p>user address: {userAddress}</p>
             <div>{reder721list}</div>
 
-            <button onClick={handlePlaceToSell}> place to sell</button>
+            {/* <button onClick={handlePlaceToSell}> place to sell</button>
             <button onClick={handleBuy}> buy </button>
-            <button onClick={handlefinalize}> finalize </button>
+            <button onClick={handlefinalize}> finalize </button> */}
             {/* <button onClick={get1155balance}> get 1155 balance </button> */}
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
