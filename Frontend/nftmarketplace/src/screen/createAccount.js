279022eeb2f11dd9e4bf8db8c3bf76839dcecabb
JasonChen990513 <149560212+JasonChen990513@users.noreply.github.com>
2024-12-17 15:57:16 +0800

    read image local

--- Frontend/nftmarketplace/src/screen/createAccount.js
+++ Frontend/nftmarketplace/src/screen/createAccount.js
@@ -39,7 +39,7 @@ const CreateAccount = () => {
             });
         
             console.log('Response:', response.data);
-
+            navigate('/login');
 
           } catch (error) {
             console.error('Error sending data:', error);
--- Frontend/nftmarketplace/src/screen/router.js
+++ Frontend/nftmarketplace/src/screen/router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter,NavLink, Outlet  } from "react-router-dom";
 import '../index.css';
 import { useActionsCreator } from "../hook/useActionsCreator";
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
 import CreateNft from "./createNft";
 import Homepage from "./homepage";
 import Nft from "./nft";
@@ -15,6 +16,88 @@ import Biding from "./biding";
 export const Root = () => {
     const { UserLogout } = useActionsCreator();
     const navigate = useNavigate();
+
+    useEffect(() => {
+        //check the network 
+        function checkNetwork(){
+            // Check if MetaMask is installed and enabled
+            if (typeof window.ethereum !== 'undefined' && window.ethereum.isMetaMask) {
+                //get current chain id
+                window.ethereum.request({ method: 'eth_chainId' })
+                .then(chainId => {
+                    // Check if the network is the maal testnet or other
+                    console.log("this is "+chainId)
+                    if (chainId !== '0x1eb4') { //0x1eb4 is maal testnet chain ID
+                        // Prompt the user to switch networks
+                        if (window.confirm('Please switch to the Maal testnet to continue.')) {
+                            SwitchChainToMaal();
+                        } else {
+                            alert("You need to switch to Maal testnet to contunue.")
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+            } else {
+                // MetaMask is not installed or not enabled
+                //alert('Please install MetaMask to use this feature.');
+                if (typeof window.ethereum === "undefined") {
+                    const confirmed = window.confirm(
+                    "MetaMask is not installed! Click OK to install it."
+                    );
+                    if (confirmed) {
+                    window.location.href = "https://metamask.io/download.html";
+                    }
+                }
+            }
+        }
+        //if not maal test net then switch to maal test net
+        async function SwitchChainToMaal(){
+            try {
+                //switch the netword to maal testnet network
+                await window.ethereum.request({
+                    method: 'wallet_switchEthereumChain',
+                    params: [{ chainId: '0x1eb4' }],
+                });
+                //after change refresh the page
+                window.location.reload();
+            } catch (switchError) {
+            if (switchError.code === 4902) { // 4902 mean the network not at wallet
+                //request to add the chain to wallet here
+                if(window.confirm("Maal testnet Chain hasn't been added to the wallet! Do you want to add it now?")){
+                    addMaalTestNetwork();
+                }
+            }
+            }
+        }
+        //if no in wallet, add network to wallet
+        async function addMaalTestNetwork() {
+            try {
+                //add maal testnet network to user wallet
+                const result = await window.ethereum.request({
+                    method: "wallet_addEthereumChain",
+                    params: [{
+                    chainId: "0x1eb4",
+                    rpcUrls: ["https://rpc-bntest.maalscan.io"],
+                    chainName: "MaalChain Testnet",
+                    nativeCurrency: {
+                        name: "MAAL",
+                        symbol: "MAAL",
+                        decimals: 18
+                    },
+                    blockExplorerUrls: ["https://testnet.maalscan.io/"]
+                    }]
+                });
+                alert("Add network successfully")
+                window.location.reload();
+            } catch (error){
+                alert("Something wrong " + error)
+            }
+        } 
+        
+        checkNetwork();
+    }, []);
 
     const handleLogOut = () => {
 		UserLogout();
